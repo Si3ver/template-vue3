2fc14e89705d709f7c24d8e7a753d588e02bd5a3
Si3ver <izhouwl@163.com>
2024-10-20 20:07:17 +0800

    chore: add federation deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,29 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    federation({
+      name: 'remote-app',
+      filename: 'remoteEntry.js',
+      // Modules to expose
+      exposes: {
+        './HelloWorld': './src/components/HelloWorld.vue',
+      },
+      shared: ['vue'],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    minify: false,
+    target: 'esnext',
+  },
 })
